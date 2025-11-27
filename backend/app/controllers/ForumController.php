db8cfffe810214dd3871c3633b5608f2c66ecba8
Rhoo <melomelodeon@gmail.com>
2025-11-28 01:52:52 +0800

    removed / in header

--- backend/app/controllers/ForumController.php
+++ backend/app/controllers/ForumController.php
@@ -9,7 +9,7 @@ class ForumController extends Controller {
         $this->call->model("ForumComment");
 
         // Headers for CORS
-        header("Access-Control-Allow-Origin: https://finalmvp-frontend.onrender.com/");
+        header("Access-Control-Allow-Origin: https://finalmvp-frontend.onrender.com");
         header("Access-Control-Allow-Headers: Content-Type, Authorization, X-Requested-With");
         header("Access-Control-Allow-Methods: GET, POST, PUT, DELETE, OPTIONS");
         header("Content-Type: application/json");
